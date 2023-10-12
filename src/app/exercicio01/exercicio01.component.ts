eef3a963b27f3cd62f49796666a2376e43904d2d
Tita-Simoni <cintiasimoni.vet@gmail.com>
2023-10-12 16:57:58 -0300

    Alterações do exercício 01

--- src/app/exercicio01/exercicio01.component.ts
+++ src/app/exercicio01/exercicio01.component.ts
@@ -1,16 +1,39 @@
 import { Component } from '@angular/core';
 
+/**
+ * Componente em Angular para o Exercício 01
+ * Este componente permite contar o número de vogais em uma palavra fornecida pelo usuário
+ */
+
 @Component({
   selector: 'app-exercicio01',
   templateUrl: './exercicio01.component.html',
   styleUrls: ['./exercicio01.component.css']
 })
+
 export class Exercicio01Component {
+  /**
+   * A palavra inserida pelo usuário
+  */
   userWord: string = '';
+
+  /**
+   * O número de vogais da palavra
+   */
   numVowels: number | undefined;
   
-  contVowels() {
+  /**
+   * Função para contar o número de vogais da palavra inserida pelo usuário
+   */
+  countVowels(): void {
+    /**
+     * Expessão redular para encontrar todas as vogais na palavra
+     */
     const vowels = this.userWord.match(/[aeiou]/gi);
+
+    /**
+     * Atribui o número de vogais à variável numVowels
+     */
     this.numVowels = vowels ? vowels.length : 0;
   }
 }
